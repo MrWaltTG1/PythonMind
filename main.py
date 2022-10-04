9fded3b50976d35797fc159b50672b32009fcfc4
MrWaltTG1 <wbesseling13@gmail.com>
2022-10-04 11:55:20 +0200

    Added gf and settings plus a screen

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def run():
+import pygame
+import game_functions as gf
+from settings import Settings
 
+def run():
+    pygame.init()
+    settings = Settings()
+    
+    screen=pygame.display.set_mode((settings.screen_width, settings.screen_height), vsync= 1)
+    pygame.display.set_caption("Pirate invasion")
     while True:
         pass
 
---
+++ settings.py
@@ -0,0 +1,6 @@
+import pygame
+
+class Settings():
+    def __init__(self):
+        self.screen_width = 800
+        self.screen_height = 800
